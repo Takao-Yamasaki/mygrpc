73d9189c1779679c934aace28a9bdfeee81526fa
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-08-08 22:27:11 +0900

    add:subnet

--- network/prd/resources.tf
+++ network/prd/resources.tf
@@ -22,24 +22,47 @@ resource "aws_internet_gateway" "myecs_igw" {
   }
 }
 
-# パブリックサブネットの作成
-resource "aws_subnet" "myecs_public" {
+# パブリックサブネットの作成(1)
+resource "aws_subnet" "myecs_public_1" {
   vpc_id = aws_vpc.myecs.id
   cidr_block = "10.0.1.0/24"
+  # 起動されるEC2には、パブリックIPが自動的に割り当てられる
   map_public_ip_on_launch = true
   
   tags = {
-    Name = "myecs_public"
+    Name = "myecs_public_1"
   }
 }
 
-# プライベートサブネットの作成
-resource "aws_subnet" "myecs_private" {
+# パブリックサブネットの作成(2)
+resource "aws_subnet" "myecs_public_2" {
   vpc_id = aws_vpc.myecs.id
   cidr_block = "10.0.2.0/24"
+  # 起動されるEC2には、パブリックIPが自動的に割り当てられる
+  map_public_ip_on_launch = true
+  
+  tags = {
+    Name = "myecs_public_2"
+  }
+}
+
+# プライベートサブネットの作成(1)
+resource "aws_subnet" "myecs_private_1" {
+  vpc_id = aws_vpc.myecs.id
+  cidr_block = "10.0.3.0/24"
 
   tags = {
-    Name = "myecs_private"
+    Name = "myecs_private_1"
+  }
+}
+
+# プライベートサブネットの作成(2)
+resource "aws_subnet" "myecs_private_2" {
+  vpc_id = aws_vpc.myecs.id
+  cidr_block = "10.0.4.0/24"
+
+  tags = {
+    Name = "myecs_private_2"
   }
 }
 
@@ -57,8 +80,14 @@ resource "aws_route_table" "myecs_route_table" {
   }
 }
 
-# パブリックサブネットとルートテーブルの関連付け
-resource "aws_route_table_association" "public" {
-  subnet_id = aws_subnet.myecs_public.id
+# パブリックサブネットとルートテーブルの関連付け(1)
+resource "aws_route_table_association" "public_1" {
+  subnet_id = aws_subnet.myecs_public_1.id
+  route_table_id = aws_route_table.myecs_route_table.id
+}
+
+# パブリックサブネットとルートテーブルの関連付け(2)
+resource "aws_route_table_association" "public_2" {
+  subnet_id = aws_subnet.myecs_public_2.id
   route_table_id = aws_route_table.myecs_route_table.id
 }
