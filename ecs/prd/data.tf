45e14a5b4bc0781ca683cbd03d58bb709f9bcce5
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-08-09 09:47:10 +0900

    fix:typo

--- ecs/prd/data.tf
+++ ecs/prd/data.tf
@@ -1,19 +1,37 @@
-# # ALBを動かしたいVPCの設定
-# data "aws_vpc" "myecs" {
-#   id = "vpc-0f081979fb819d695"
-# }
+# ALBを動かしたいVPCの設定
+data "aws_vpc" "myecs" {
+  id = "vpc-07675977ef37e3850"
+}
 
-# # ALBを動かしたいパブリックサブネットの設定
-# data "aws_subnets" "myecs_public" {
-#   vpc_id = "vpc-0f081979fb819d695"
-# }
+# ALBを動かしたいパブリックサブネットの設定
+# TypeタグがPublicのサブネットをフィルタリング
+data "aws_subnets" "myecs_public" {
+  # VPCIDを元にサブネットをフィルタリング
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.myecs.id]
+  }
 
-# # # ALBを動かしたいプライベートサブネットの設定
-# data "aws_subnets" "myecs_private" {
-#   id = "subnet-0d82c9806de5a8f43"
-# }
+  filter {
+    name = "tag:Type"
+    values = ["Public"]
+  }
+}
 
-# # ACM証明書
+# ALBを動かしたいプライベートサブネットの設定
+# TypeタグがPrivateのサブネットをフィルタリング
+data "aws_subnets" "myecs_private" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.myecs.id]
+  }
+  filter {
+    name = "tag:Type"
+    values = ["Private"]
+  }
+}
+
+# ACM証明書
 data "aws_acm_certificate" "myecs" {
   domain = "zackzack.link"
   # 有効な証明書のみを検索
@@ -22,22 +40,26 @@ data "aws_acm_certificate" "myecs" {
 
 # ECRリポジトリ
 data "aws_ecr_repository" "myecs" {
-  name = "mygrpc"
+  name = "myecs"
 }
 
+# base_name
 variable "base_name" {
   type = string
   default = "myecs"
 }
 
-# # ALBを動かしたいVPCの設定
-# variable "id" {
-#   type = string
-#   default = "vpc-0f081979fb819d695"
-# }
+# リージョン
+variable "region" {
+  type = string
+  default = "ap-northeast-1"
+}
 
-# # ALBを動かしたいパブリックサブネットの設定
-# variable "vpc_id" {
-#   type = string
-#   default = "vpc-0f081979fb819d695"
-# }
+# タグ
+variable "image_tag" {
+  type = string
+  default = "latest"
+}
+
+
+# TODO: 証明書の設定
